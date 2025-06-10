4d0eeb710b53aa59448e8f73e829156a537417a3
lsy881109 <hotppm@naver.com>
2025-06-10 17:36:11 +0900

    퇴실전 저장

--- d250609/ch5_3/PhoneMainEx1_5.java
+++ d250609/ch5_3/PhoneMainEx1_5.java
@@ -3,7 +3,7 @@ package d250609.ch5_3;
 public class PhoneMainEx1_5 {
     public static void main(String[] args) {
         // 기본 인스턴스 2개 생성 후, 정보 확인.
-        IphoeEx1_4 iphoeEx1_4 = new IphoeEx1_4("애플", "아이폰 16", "약200만원", "서면롯데백화점");
+        IphoneEx1_4 iphoeEx1_4 = new IphoneEx1_4("애플", "아이폰 16", "약200만원", "서면롯데백화점");
         iphoeEx1_4.showInfo();
 
         GalaxyEx1_3 galaxyEx1_3 = new GalaxyEx1_3("삼성", "갤럭시25", "약200만원", "삼성로고");
@@ -13,12 +13,12 @@ public class PhoneMainEx1_5 {
         // 갤럭시 폰이다, 아이폰 폰이다. -> Is A 관계 , 상속 관계,
         // 갤럭시, 아이폰, 모두, 폰 타입으로 받아도 될까요?
         // 부모 클래스 자식 클래스, -> 이 인스턴의 타입은 무엇이냐? 부모 클래스 타입.PhoneEx1_2
-        PhoneEx1_2 phoneIphone = new IphoeEx1_4("애플", "아이폰16", "약200만원", "프리비스");
+        PhoneEx1_2 phoneIphone = new IphoneEx1_4("애플", "아이폰16", "약200만원", "프리비스");
         // 형 확인하는 연산자 이용해보기. -> 원래의 타입을 확인하는 절차.
-        if (phoneIphone instanceof IphoeEx1_4) {
+        if (phoneIphone instanceof IphoneEx1_4) {
             // 다운 캐스팅 : 위 -> 아래 감. 부모 클래스에서 -> 자식 클래스 타입으로 변환.
             // 원래대로 본인의 형으로 변환함.
-            IphoeEx1_4 iphoeEx1_42 = (IphoeEx1_4) phoneIphone;
+            IphoneEx1_4 iphoeEx1_42 = (IphoneEx1_4) phoneIphone;
             // 아이폰에 만 있는 기능.
             System.out.println("형이 아이폰 클래스 형이 맞다면, 여기에만 있는 기능 확인");
             iphoeEx1_42.sellInfo();
@@ -48,10 +48,10 @@ public class PhoneMainEx1_5 {
         // 그래서, 원래 타입으로 변경하고, 사용함.
         // 막 , 아무타입으로 변경하면 안되니, 그래서 형을 확인하고서 변환을함.
 
-        if (samsungPhone instanceof IphoeEx1_4) {
+        if (samsungPhone instanceof IphoneEx1_4) {
             // 다운 캐스팅 : 위 -> 아래 감. 부모 클래스에서 -> 자식 클래스 타입으로 변환.
             // 원래대로 본인의 형으로 변환함.
-            IphoeEx1_4 iphoeEx1_42 = (IphoeEx1_4) samsungPhone;
+            IphoneEx1_4 iphoeEx1_42 = (IphoneEx1_4) samsungPhone;
             // 아이폰에 만 있는 기능.
             System.out.println("형이 아이폰 클래스 형이 맞다면, 여기에만 있는 기능 확인");
             iphoeEx1_42.sellInfo();
