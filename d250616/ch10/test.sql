b88b63b666848c0cc0aeb935b325e3a848e79ed1
lsy881109 <hotppm@naver.com>
2025-06-16 16:48:41 +0900

    퇴실전 저장

---
+++ d250616/ch10/test.sql
@@ -0,0 +1,46 @@
+-- 샘플 시퀀스 만들기. 
+create sequence member501_seq start with 1 increment by 1;
+
+drop sequence member501_seq; // 삭제
+drop table member501; // 삭제
+--샘플 확인용 테이블 만들기
+create table member501 (
+   id       number primary key,
+   name     varchar2(100),
+   password varchar2(100),
+   email    varchar2(100),
+   reg_date varchar2(50)
+)
+-- 샘플 데이터 하나 넣기. 
+insert into member501 (
+   id,
+   name,
+   password,
+   email,
+   reg_date
+) values ( member501_seq.nextval,
+           '이상용',
+           '1234',
+           'lsy@naver.com',
+           '2025년6월16일12시9분' );
+
+select sequence_name
+  from user_sequences; --조회z
+
+drop sequence membber501_seq; -- 시퀀스 삭제
+
+select *
+  from member501
+ where id = 2;
+
+select *
+  from member501
+ where name like '%이상용%';
+
+
+
+commit;
+
+-- 테이블 조회 
+select *
+  from member501;
